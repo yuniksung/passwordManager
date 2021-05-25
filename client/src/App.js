52d58c1b7109939625e904028736bfe12268ad46
Joshua Sung <yuniksung@gmail.com>
2021-05-25 00:24:44 -0500

    front end added 'simple layout'

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <div className="AddingPassword">
+       <input type="text" placeholder="Ex. password123" onChange={(event) => {
+         setPassword(event.target.value);
+       }}/>
+       <input type="text" placeholder="Ex. Facebook" onChange={(event) => {
+         setTitle(event.target.value);
+       }}/>
+       <button>
+         Add Password
+       </button>
+     </div>
+   </div>
   );
 }
 
