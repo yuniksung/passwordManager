7c1be33ad07f354a3c774a2197c5b9db1bcb8bdb
Joshua Sung <yuniksung@gmail.com>
2021-05-24 23:20:56 -0500

    schema created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mysql = require("mysql");
 const PORT = 3001;
 
+dotenv.config();
+ 
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: process.env.PASSWORD,
+    database: 'passwordmanager',
+})
+
 app.listen(PORT, ()=> {
-    console.log('Server is running')
+    console.log(`Server is running: http://localhost:${PORT}/`)
 })
